82e2377c15d88001e1965a4e3a4cb27a933dd600
Alex Liang <alexliang@u.boisestate.edu>
2022-10-23 20:53:01 -0600

    webpage up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+
+  get "/applycss", to: "articles#applycss"
+  get "/applyhtml", to: "articles#applyhtml"
+  get "/generator", to: "articles#generator"
+  get "/scraper", to: "articles#scraper"
 end
