1e76f96904c1b8cbdc7002be23cc8761fddf0189
shuai-yang <shuaiyang@u.boisestate.edu>
2022-11-06 15:01:27 -0700

    correct pages of scraping_sample

--- app/controllers/scraping_sample.rb
+++ app/controllers/scraping_sample.rb
@@ -3,27 +3,30 @@
 ###############
 require 'httparty'
 require 'nokogiri'
-require 'open-uri'
+#require 'csv'
 
 def scraping(url)
-    books = []
-    50.times do |i|
-        url = url+"/catalogue/page-#{i + 1}.html"
-        response = HTTParty.get(url)
-        # parse HTML with Nokogiri
-        document = Nokogiri::HTML4(response.body)
-        # each book is contained in the HTML element article with its class set to product_pod
-        books_containers = document.css('article.product_pod')
+    #CSV.open('books.csv', 'w+', write_headers: true, headers: %w[Title Price Availability]) do |csv|
         books = []
-        books_containers.each do |container|
-            title = container.css('h3 a').first['title']
-            price = container.css('.price_color').text.delete('^0-9.')
-            availability = container.css('.availability').text.strip
-            book = [title, price, availability]
-            print book
-            books << book 
-        end        
-    end
+        50.times do |i|
+            page_url = url + "catalogue/page-#{i + 1}.html"  
+            # send an HTTP GET request
+            response = HTTParty.get(page_url)
+            # parse HTML with Nokogiri
+            document = Nokogiri::HTML4(response.body)
+            # each book is contained in the HTML element article with its class set to product_pod
+            books_containers = document.css('article.product_pod')
+            books_containers.each do |container|
+                title = container.css('h3 a').first['title']
+                price = container.css('.price_color').text.delete('^0-9.')
+                availability = container.css('.availability').text.strip
+                book = [title, price, availability]
+                #csv << book
+                books << book 
+            end    
+        end
+        puts "#{books}\n"
+    #end
 end
 
 scraping('https://books.toscrape.com/')
