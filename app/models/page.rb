7ba315d4af16920d9eb52c3fac1f097f64defa41
jeremystocking <68076394+jeremystocking@users.noreply.github.com>
2022-11-05 23:30:55 -0600

    Updated App file

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,6 @@
 class Page < ApplicationRecord
+	validates :title, presence: true
+	validates :bgColor, presence: true
+	validates :fontColor, presence: true
+	validates :fontStyle, presence: true
 end
