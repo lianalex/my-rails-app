e4d96d4d871c0ea370d705c9e8b914dce156ca4e
jeremystocking <68076394+jeremystocking@users.noreply.github.com>
2022-11-05 23:33:35 -0600

    updated test and vendor

--- test/models/page_test.rb
+++ test/models/page_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class ArticlesControllerTest < ActionDispatch::IntegrationTest
+class PageTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
