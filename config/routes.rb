3913ac9be5d990ebb8a5415952091794cefc9d84
jeremystocking <68076394+jeremystocking@users.noreply.github.com>
2022-11-05 23:31:38 -0600

    Updated config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-
+  
   root "pages#index"
-
-  get "/applycss", to: "pages#applycss"
-  get "/applyhtml", to: "pages#applyhtml"
-  get "/generator", to: "pages#generator"
-  get "/scraper", to: "pages#scraper"
+  
+  resources :pages
 end
