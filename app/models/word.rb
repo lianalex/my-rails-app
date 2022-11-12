1e83555f4e2f3385451bf9b8b0c0285351684080
ButlerMW <butler.m.w@outlook.com>
2022-11-12 12:13:05 -0700

    Added Subtitles and image uploader

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,9 @@
 class Word < ApplicationRecord
+  has_one_attached :image
   validates :title, presence: true
   validates :p1, presence: true, length: { minimum: 10 }
+  validates :subtitle1, presence: true
   validates :p2, presence: true, length: { minimum: 10 }
+  validates :subtitle2, presence: true
   validates :p3, presence: true, length: { minimum: 10 }
 end
--- db/migrate/20221108161555_create_words.rb
+++ db/migrate/20221108161555_create_words.rb
@@ -1,10 +1,13 @@
 class CreateWords < ActiveRecord::Migration[7.0]
   def change
     create_table :words do |t|
+      t.string :image
       t.string :title
       t.text :p1
+      t.string :subtitle1
       t.text :p2
-	  t.text :p3
+      t.string :subtitle2
+	    t.text :p3
 
       t.timestamps
     end
