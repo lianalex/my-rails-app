6231e914b8930857e27defca75470fbf39132e1c
ButlerMW <butler.m.w@outlook.com>
2022-11-12 13:01:59 -0700

    fixed image bug made subtitles optional

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,8 +2,8 @@ class Word < ApplicationRecord
   has_one_attached :image
   validates :title, presence: true
   validates :p1, presence: true, length: { minimum: 10 }
-  validates :subtitle1, presence: true
+  validate :subtitle1
   validates :p2, presence: true, length: { minimum: 10 }
-  validates :subtitle2, presence: true
+  validate :subtitle2
   validates :p3, presence: true, length: { minimum: 10 }
 end
