b74428cbff81a06905f4a17c1e2aa78497991c16
jeremystocking <68076394+jeremystocking@users.noreply.github.com>
2022-11-16 09:15:15 -0700

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -39,13 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_011524) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "pages", force: :cascade do |t|
     t.string "title"
     t.string "bgColor"
