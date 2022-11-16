dd977ef76b5b5679ac5563b3adee16b5ede0002a
jeremystocking <68076394+jeremystocking@users.noreply.github.com>
2022-11-15 18:07:29 -0700

    config db and gemfile updates

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/words", to: "words#index"
   get "/products", to: "products#index"
   get "/scrapes", to: "scrapes#index"
-  
+
   resources :products
   resources :pages
   resources :words
